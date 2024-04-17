aeb9c6ef6c17ebee9fbe68672303a272c0e7464e
dasun dhammasara <dasundhammasara9@gmail.com>
2024-04-17 14:31:56 +0530

    home sreec responsiveness updated

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,15 +1,15 @@
 import React from "react";
 import '../App.css';
-import '../styles.css';
+import '../home.css';
 
 export default function Home() {
   return (
     <div className="home-background">
       <div className="imageContainer">
         <div className="image imageOne"></div>
-        <div className="image imageTwo"></div>
+        {/* <div className="image imageTwo"></div>
         <div className="image imageThree"></div>
-        <div className="image imageFour"></div>
+        <div className="image imageFour"></div> */}
         <div className="discoverNext">
           <h1>Explore the Gaming Universe!</h1>
         </div>
